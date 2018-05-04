dd74513ee8464bded79991f3a707e4d061e8d7b7
Aleksandr <aleksandrbartov@yandex.ru>
2018-05-05 03:18:34 +0400

    I have begin to work with Mosh

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
@@ -6,10 +7,11 @@ const rentals = require('./routes/rentals');
 const express = require('express');
 const app = express();
 
-mongoose.connect('mongodb://localhost/vidly')
+mongoose.connect('mongodb://AleksBartov:Merahba2018@ds247439.mlab.com:47439/aleksbartovworks')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
